0743ef2020da385f6a23b656d0bb691e501ce18b
nadiduno <nadiduno@gmail.com>
2022-12-29 23:54:49 -0300

    TestNode

---
+++ main.js
@@ -0,0 +1,9 @@
+class Conta{
+
+    constructor(agencia,conta,dac){
+        this.agencia=agencia;
+        this.conta=conta;
+        this.dac=dac;
+    }
+}
+module.exports = Conta;
---
+++ spec/support/main.spec.js
@@ -0,0 +1,8 @@
+const Conta = require('../main');
+
+describe('Testando o objeto conta', () => {
+    it('Should init whith correct parameters', () =>{
+        const conta = new Conta ('1234','123','0');
+        expect (conta.agencia).toEqual('1234')
+    })
+})
